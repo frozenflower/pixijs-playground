0db3ff8a3dd24949cdec4bc04b85cea12e7868b3
Dmitry Glotov <windswept.willow@yandex.ru>
2024-09-21 12:12:02 +0300

    Init pixi.js

---
+++ src/global.d.ts
@@ -0,0 +1,5 @@
+import { Application } from "pixi.js";
+
+export declare global {
+    var __PIXI_APP__: Application;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
+import { Application, Assets, Sprite } from 'pixi.js'
 import './style.css'
+
+const app = new Application();
+
+globalThis.__PIXI_APP__ = app;
+
+await app.init({
+  backgroundColor: 'white'
+});
+
+document.body.appendChild(app.canvas);
+
+const texture = await Assets.load('Walking_0001.png');
+
+const person = Sprite.from(texture);
+
+person.scale.set(4, 4);
+
+person.anchor.set(0.5);
+
+person.x = app.renderer.width / 2;
+person.y = app.renderer.height / 2;
+
+app.stage.addChild(person);
